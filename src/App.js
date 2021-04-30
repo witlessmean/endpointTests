2f4d42a76c477054a611f2321a787a9f5f85aed2
Shawn Elston <elstonshawn1@yahoo.com>
2021-04-30 13:27:02 -0700

    posting tests

--- src/App.js
+++ src/App.js
@@ -1,52 +1,72 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  
-  const [whatWeGet, setWhatWeGet] = useState('random');
+  const [whatWeGet, setWhatWeGet] = useState("random");
 
   axios.get("http://localhost:8080/").then((stuff) => {
-    console.log(stuff)
+    console.log(stuff);
   });
-  
-const [formState, setFormState] = useState({
-  title: "",
-  description: "",
-  link: "",
-});
 
-const onInputChange = (e) => {
-  e.persist();
-  const obj = {
-    ...formState,
-    [e.target.name]: e.target.value
-  }
-  setFormState(obj)
-};
+  const [formState, setFormState] = useState({
+    title: "",
+    description: "",
+    link: "",
+  });
 
-const postSubmit = (e) => {
-  e.preventDefault();
-  axios.post("http://localhost:8080/", {
-    formState
-  }).then((stuff) => {
-    console.log(stuff)
-  }).catch((err) => {
-    console.log(err)
-  })
-};
+  const onInputChange = (e) => {
+    e.persist();
+    const obj = {
+      ...formState,
+      [e.target.name]: e.target.value,
+    };
+    setFormState(obj);
+  };
+
+  const postSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/", {
+        title: formState.title,
+        description: formState.description,
+        link: formState.link
+      })
+      .then((stuff) => {
+        console.log(stuff);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
-<form onSubmit={postSubmit}>
-  <input type='text' name="title" placeholder="title" onChange={onInputChange} value={formState.title} />
-  <input type='text' name="description" placeholder="description" onChange={onInputChange} value={formState.description} />
-  <input type='text' name="link" placeholder="link" onChange={onInputChange} value={formState.link} />
-<button type="submit">submit</button>
-</form>
-    <h1>
-       {whatWeGet}
-    </h1>
-  </>
+      <form onSubmit={postSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          onChange={onInputChange}
+          value={formState.title}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          onChange={onInputChange}
+          value={formState.description}
+        />
+        <input
+          type="text"
+          name="link"
+          placeholder="link"
+          onChange={onInputChange}
+          value={formState.link}
+        />
+        <button type="submit">submit</button>
+      </form>
+      <h1>{whatWeGet}</h1>
+    </>
   );
 }
 
