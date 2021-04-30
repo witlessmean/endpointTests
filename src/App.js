b512bf841ed1d5596e621632c35282feb542785a
Shawn Elston <elstonshawn1@yahoo.com>
2021-04-29 21:24:10 -0700

    simple testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div>
+  
+  const [whatWeGet, setWhatWeGet] = useState('random');
 
-    </div>
+  axios.get("http://localhost:8080/").then((stuff) => {
+    console.log(stuff)
+  });
+  
+const [formState, setFormState] = useState({
+  title: "",
+  description: "",
+  link: "",
+});
+
+const onInputChange = (e) => {
+  e.persist();
+  const obj = {
+    ...formState,
+    [e.target.name]: e.target.value
+  }
+  setFormState(obj)
+};
+
+const postSubmit = (e) => {
+  e.preventDefault();
+  axios.post("http://localhost:8080/", {
+    formState
+  }).then((stuff) => {
+    console.log(stuff)
+  }).catch((err) => {
+    console.log(err)
+  })
+};
+
+  return (
+    <>
+<form onSubmit={postSubmit}>
+  <input type='text' name="title" placeholder="title" onChange={onInputChange} value={formState.title} />
+  <input type='text' name="description" placeholder="description" onChange={onInputChange} value={formState.description} />
+  <input type='text' name="link" placeholder="link" onChange={onInputChange} value={formState.link} />
+<button type="submit">submit</button>
+</form>
+    <h1>
+       {whatWeGet}
+    </h1>
+  </>
   );
 }
 
